0a405f928e16d861845afa439ebc7c0895c0d67e
Mateus Cruz <makteus@gmail.com>
2021-03-19 15:23:34 -0300

    Add rspec dependencies

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+ruby '2.6.5'
+
+source 'https://rubygems.org'
+
+group :test do
+  gem 'rspec', '~> 3.10'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.configure do |config|
+  config.color     = true
+  config.formatter = :documentation
+  config.order     = :random
+end
